eb46fee23c765f7e4e4469dc30bd54c4684ab3c4
Ko Myo <myothantktt@gmail.com>
2017-03-11 16:07:03 +0630

    Gallery view demo using Dialog

---
+++ GalleryView/app/src/main/java/com/ps/galleryview/MainActivity.java
@@ -0,0 +1,76 @@
+package com.ps.galleryview;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+/*
+*Source From:
+*http://stacktips.com/tutorials/android/android-gridview-example-building-image-gallery-in-android
+*/
+
+public class MainActivity extends ActionBarActivity {
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+    private Button showDBtn;
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showDBtn = (Button) findViewById(R.id.showDId);
+
+        showDBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.activity_dialog);
+
+                gridView = (GridView) dialog.findViewById(R.id.gridView);
+                gridAdapter = new GridViewAdapter(context, R.layout.grid_item_layout, getData());
+                gridView.setAdapter(gridAdapter);
+
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                        ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+
+                        //Create intent
+                        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                        intent.putExtra("title", item.getTitle());
+                        intent.putExtra("image", item.getImage());
+
+                        //Start details activity
+                        startActivity(intent);
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+
+    /**
+     * Prepare some dummy data for gridview
+     */
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
+    }
+}
