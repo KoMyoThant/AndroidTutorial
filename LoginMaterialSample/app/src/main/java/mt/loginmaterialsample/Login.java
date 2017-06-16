79e17a1c288096e8b5887d4f0f1473625fd02c18
KoMyoThant <myothantktt@gmail.com>
2017-06-17 01:06:14 +0630

    Material design login sample

---
+++ LoginMaterialSample/app/src/main/java/mt/loginmaterialsample/Login.java
@@ -0,0 +1,17 @@
+package mt.loginmaterialsample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by panacea on 6/17/17.
+ */
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
