d783ce60c8dc350d242f657d07bcc74b0fe11dad
KoMyoThant <myothantktt@gmail.com>
2017-06-17 01:55:52 +0630

    Login process

--- LoginMaterialSample/app/src/main/java/mt/loginmaterialsample/Login.java
+++ LoginMaterialSample/app/src/main/java/mt/loginmaterialsample/Login.java
@@ -1,17 +1,30 @@
 package mt.loginmaterialsample;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by panacea on 6/17/17.
  */
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
+    private TextInputLayout emailTIL, pwdTIL;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        emailTIL = (TextInputLayout) findViewById(R.id.emailTILId);
+        pwdTIL = (TextInputLayout) findViewById(R.id.pwdTILId);
+    }
+
+    public boolean checkValidate() {
+        boolean validate = true;
+
+        return validate;
     }
 }
